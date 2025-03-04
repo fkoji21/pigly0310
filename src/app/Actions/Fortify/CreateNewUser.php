117a2484b823fb7af360321ab3328a3cea2d9a5c
fujita koji <fcreation.net@gmail.com>
2025-03-04 10:45:44 +0900

    余分なリダイレクトを削除

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -37,7 +37,5 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
-
-        return redirect('/register/step2');
     }
 }
